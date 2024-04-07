1bea7d06eb392648d87d3523e9ffb55bd4606d6c
Nikita <glushkov1520@gmail.com>
2024-04-07 19:21:00 +0300

    Initial commit

---
+++ src/main/java/com/mycompany/selectionhandler/ManagementController.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.selectionhandler;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author 79175
+ */
+public class ManagementController {
+    private final InputReader inputReader = new InputReader();
+    private final OutputWriter outputWriter = new OutputWriter();
+    private final CalculationModule calculationModule = new CalculationModule();
+    private ArrayList<Double[]> selection = new ArrayList<>();
+    
+}
---
+++ src/main/java/com/mycompany/selectionhandler/SelectionHandler.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.selectionhandler;
+
+/**
+ *
+ * @author 79175
+ */
+public class SelectionHandler {
+
+    public static void main(String[] args) {
+        ManagementController managementController = new ManagementController();
+    }
+}
